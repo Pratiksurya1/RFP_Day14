ca202ce56928821701060f64f04198f0dee20157
Pratiksurya1 <suryawanshipratik356@gmail.com>
2022-01-21 09:55:22 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkdList_Test
+{
+    internal class Program
+    {
+        public static void Main(String[] args)
+        {
+            LinkedList number = new LinkedList();
+            number.Add(56);
+            number.Add(30);
+            number.Add(70);
+        }
+    }
+}
